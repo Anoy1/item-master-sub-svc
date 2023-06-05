f8181f468daf47d3a437cf80d921855e74d39dcc
amianoybiswas <amianoybiswas@gmail.com>
2023-06-06 01:25:39 +0530

    ANOY | SUBSCRIBER CHANGES

--- src/main/java/com/anoy/load/itemmaster/sub/service/ItemMasterSubService.java
+++ src/main/java/com/anoy/load/itemmaster/sub/service/ItemMasterSubService.java
@@ -1,15 +1,18 @@
 package com.anoy.load.itemmaster.sub.service;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ItemMasterSubService {
 
 @KafkaListener(topics = "itemmaster", groupId = "group_id")
-public void consume(String message)
+public void consume(ConsumerRecord<String, String> data)
 {
- System.out.println("message = " + message);
+ System.out.println("message = " + data.value().toString());
+ //acknowledgment.acknowledge();
 }
 	public void processData() {
 		
